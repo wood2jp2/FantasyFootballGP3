57da2a85d48632755b38700437417bc9fec55f30
Josh Wood <wood2jp2@gmail.com>
2017-10-16 13:49:46 -0400

    adding functional react login signup pages...

---
+++ client/src/components/Signup.js
@@ -0,0 +1,35 @@
+// import React, { Component } from "react";
+// import API from "../utils/API";
+//
+// class SignupComponent extends React.Component {
+//
+//   state = {
+//     username: '',
+//     password: '',
+//     email: ''
+//   }
+//
+//   handleChange = e => {
+//     this.setState({
+//       [e.target.name]: e.target.value
+//     })
+//   }
+//
+//   render() {
+//     <div>
+//     <input
+//       name='username'
+//       value={this.state.username}
+//       onChange= {this.handleChange}
+//     />
+//    <input
+//       name='password'
+//       value={this.state.password}
+//       onChange= {this.handleChange}
+//    />
+//    <button onClick={() => {
+//      this.attemptSignup
+//    }}> Signup! </button>
+//    </div>
+//   }
+// }
