208ea6f4263677d91f3ab794b93cd6b8e67da5c0
HERNAN TEJADA <hernan.tejada@gmail.com>
2017-10-16 16:27:00 -0400

    Update App.js
    Conflicts has just been fixed on this file.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,8 @@
 import React from "react";
-<<<<<<< HEAD
-import PlayersSearch from "./components/PlayersSearch";
 // import SignupComponent from './components/Signup';
 import SigninComponent from './components/Signin';
-=======
 import PlayersSearch from "./components/Search/PlayersSearch";
->>>>>>> 07d78008f37e6122397b5323bb20e3089e8c1c69
+
 
 const App = () => {
   return(
