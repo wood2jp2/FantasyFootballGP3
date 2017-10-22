15bce1e5fb73c6482ef8786beb5e83d5f74d65c5
Josh Wood <wood2jp2@gmail.com>
2017-10-22 19:25:48 -0400

    scrape scripts complete

--- client/src/components/Redirect.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from "react";
-
-const Redirect = () => {
-  this.props.history.push('/signin');
-}
-
-export default Redirect;
---
+++ client/src/server.js
@@ -0,0 +1,57 @@
+const
+  express = require('express'),
+  mongoose = require('mongoose'),
+  bodyParser = require('body-parser'),
+  request = require('request'),
+  cheerio = require('cheerio'),
+  localServer = "mongodb://localhost:27017/InjuryScrape1",
+  port = process.env.PORT || 3000,
+  InjuryUpdate = require('./MongoSearch/model/InjuryUpdate.js'),
+  app = express(),
+  db = mongoose.connection;
+
+mongoose.promise = Promise;
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.use(express.static("public"));
+
+// test scraping link, from what I understand, this temporarily hosts your mongoose connection
+mongoose.connect(localServer, {
+  useMongoClient: true
+});
+
+app.listen(port, function() {
+  console.log('App running on port 3000')
+});
+
+app.get('/scrape', function(req, res) {
+  console.log('scrape scrape');
+  request('http://www.espn.com/nfl/injuries', function(err, res, html) {
+    const $ = cheerio.load(html);
+    $('.oddrow,.evenrow').each(function(i, element) {
+      var result = {};
+      if (i % 2 === 0) {
+        result.name=$(this).find('a').text();
+        positionIsolation=$(this).find('td:first-of-type').text().split(' ');
+        result.position=positionIsolation[positionIsolation.length-1];
+        result.status=$(this).find('td:nth-child(2)').text();
+        result.date=$(this).find('td:last-child').text();
+
+        var entry = new InjuryUpdate(result);
+
+        entry.save(function(err, doc) {
+          if (err) {
+            console.log(err)
+          } else {
+            console.log(doc)
+          }
+        });
+      }
+
+    });
+  });
+  res.send('scrape complete')
+})
